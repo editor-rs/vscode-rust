51db6838c11327cc1e50902b95ec614e4a2e1d1f
Konstantin Akhantev <saviorisdead@gmail.com>
2015-11-21 16:16:59 +0300

    Working formatting

--- src/rustFormat.ts
+++ src/rustFormat.ts
@@ -10,34 +10,37 @@ function formatRustfmtCommand(fileName: string, writeMode: string): string {
 export class RustDocumentFormattingEditProvider implements vscode.DocumentFormattingEditProvider {
 	private writeMode: string = 'display';
 	
+	constructor() {
+		this.writeMode = 'display';
+	}
+	
 	public provideDocumentFormattingEdits(document: vscode.TextDocument, 
 										  options: vscode.FormattingOptions, 
 										  token: vscode.CancellationToken): Thenable<vscode.TextEdit[]> {
 		return document.save().then(() => {
-			return this.performFormatFile(document, options, token);
+			return this.performFormatFile(document, options, token, this.writeMode);
 		});
 	}
 	
 	private performFormatFile(document: vscode.TextDocument, 
 							  options: vscode.FormattingOptions, 
-							  token: vscode.CancellationToken) : Promise<vscode.TextEdit[]> {
+							  token: vscode.CancellationToken,
+							  writeMode: string) : Promise<vscode.TextEdit[]> {
 		return new Promise(function (resolve, reject) {
 			let fileName = document.fileName;
-			
-			let command = formatRustfmtCommand(fileName, this.writeMode);
-			
-			console.log(command);
+			let command = formatRustfmtCommand(fileName, writeMode);
 			
 			cp.exec(command, (err, stdout, stderr) => {
 				try {
 					if (err && (<any>err).code == 'ENOENT') {
-						vscode.window.showInformationMessage('The "rustfmt" command is not available.');
+						vscode.window.showInformationMessage('The "rustfmt" command is not available. Make sure it is installed.');
 						return resolve(null);
 					}
 					if (err) return reject('Cannot format due to syntax errors');
 					
-					let text = stdout.toString();
-					console.log(text);
+					// Need this to remove label of rustfmt output
+					let text = stdout.toString().split('\n').slice(2).join('\n');
+					
 					//TODO: implement parsing of rustfmt output with 'diff' writemode
 					let lastLine = document.lineCount;
 					let lastLineLastCol = document.lineAt(lastLine - 1).range.end.character;
