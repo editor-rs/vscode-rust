447447aff26f029f37ab20e12510319a5353684a
Alexey <kalita.alexey@outlook.com>
2017-02-14 21:36:07 +0400

    Fixed constructing the path for a diagnostic. (#87)

--- src/components/cargo/diagnostic_publisher.ts
+++ src/components/cargo/diagnostic_publisher.ts
@@ -1,4 +1,4 @@
-import { join } from 'path';
+import { isAbsolute, join } from 'path';
 
 import { Diagnostic, DiagnosticCollection, Uri, languages } from 'vscode';
 
@@ -19,13 +19,14 @@ export class DiagnosticPublisher {
      * Publishes a diagnostic if the diagnostic wasn't published yet
      */
     public publishDiagnostic(fileDiagnostic: FileDiagnostic, cwd: string): void {
-        const diagnostic = fileDiagnostic.diagnostic;
-        const filePath = Uri.file(join(cwd, fileDiagnostic.filePath));
+        const { diagnostic, filePath } = fileDiagnostic;
+        const absoluteFilePath = isAbsolute(filePath) ? filePath : join(cwd, filePath);
+        const filePathUri = Uri.file(absoluteFilePath);
 
-        const oneFileDiagnostics = this.diagnostics.get(filePath);
+        const oneFileDiagnostics = this.diagnostics.get(filePathUri);
 
         if (oneFileDiagnostics === undefined) {
-            this.diagnostics.set(filePath, [diagnostic]);
+            this.diagnostics.set(filePathUri, [diagnostic]);
         } else if (this.isUniqueDiagnostic(diagnostic, oneFileDiagnostics)) {
             oneFileDiagnostics.push(diagnostic);
         }
