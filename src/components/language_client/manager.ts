79094909601857dcc883411d54581cc0fbc1811c
Kalita Alexey <kalita.alexey@outlook.com>
2017-02-20 17:05:07 +0300

    Click on the status bar item to restart RLS

--- src/components/language_client/manager.ts
+++ src/components/language_client/manager.ts
@@ -1,4 +1,4 @@
-import { ExtensionContext } from 'vscode';
+import { Disposable, ExtensionContext, window, workspace } from 'vscode';
 
 import { LanguageClient, State } from 'vscode-languageclient';
 
@@ -15,8 +15,6 @@ export class Manager {
 
     private statusBarItem: StatusBarItem;
 
-    private context: ExtensionContext;
-
     private logger: ChildLogger;
 
     public constructor(
@@ -28,22 +26,67 @@ export class Manager {
     ) {
         this.languageClientCreator = new LanguageClientCreator(executable, args, env);
 
-        this.context = context;
+        this.languageClient = this.languageClientCreator.create();
 
         this.statusBarItem = new StatusBarItem(context);
 
+        this.statusBarItem.setOnClicked(() => {
+            this.restart();
+        });
+
         this.logger = logger;
 
-        this.languageClient = this.languageClientCreator.create();
+        this.subscribeOnStateChanging();
+
+        context.subscriptions.push(new Disposable(() => {
+            this.stop();
+        }));
     }
 
     public start(): void {
         this.logger.debug('start');
 
-        this.languageClient.outputChannel.show();
+        this.languageClient.start();
 
+        this.statusBarItem.updateVisibility();
+
+        this.statusBarItem.enable();
+    }
+
+    public stop(): void {
+        this.logger.debug('stop');
+
+        this.statusBarItem.disable();
+
+        if (this.languageClient.needsStop()) {
+            this.languageClient.stop();
+        }
+    }
+
+    /** Stops the running language client if any and starts a new one. */
+    private restart(): void {
+        const isAnyDocumentDirty = !workspace.textDocuments.every(t => !t.isDirty);
+
+        if (isAnyDocumentDirty) {
+            window.showErrorMessage('You have unsaved changes. Save or discard them and try to restart again');
+
+            return;
+        }
+
+        this.stop();
+
+        this.languageClient = this.languageClientCreator.create();
+
+        this.subscribeOnStateChanging();
+
+        this.start();
+    }
+
+    private subscribeOnStateChanging(): void {
         this.languageClient.onDidChangeState(event => {
             if (event.newState === State.Running) {
+                this.languageClient.outputChannel.show();
+
                 this.languageClient.onNotification('rustDocument/diagnosticsBegin', () => {
                     this.statusBarItem.setText('Analysis started');
                 });
@@ -51,11 +94,9 @@ export class Manager {
                 this.languageClient.onNotification('rustDocument/diagnosticsEnd', () => {
                     this.statusBarItem.setText('Analysis finished');
                 });
+            } else {
+                this.statusBarItem.setText('Stopped');
             }
         });
-
-        this.context.subscriptions.push(this.languageClient.start());
-
-        this.statusBarItem.updateVisibility();
     }
 }
--- src/components/language_client/status_bar_item.ts
+++ src/components/language_client/status_bar_item.ts
@@ -1,20 +1,51 @@
 import * as vscode from 'vscode';
 
-import { ExtensionContext, languages, window } from 'vscode';
+import { ExtensionContext, commands, languages, window } from 'vscode';
 
 import getDocumentFilter from '../configuration/mod';
 
+const command = 'rust.LanguageClient.StatusBarItem.Clicked';
+
 export class StatusBarItem {
     private statusBarItem: vscode.StatusBarItem;
+    private onClicked?: () => void;
 
     public constructor(context: ExtensionContext) {
         this.statusBarItem = window.createStatusBarItem();
+        this.statusBarItem.tooltip = 'Click to restart';
+
+        context.subscriptions.push(
+            commands.registerCommand(command, () => {
+                if (this.onClicked !== undefined) {
+                    this.onClicked();
+                }
+            })
+        );
 
         context.subscriptions.push(
             window.onDidChangeActiveTextEditor(() => {
                 this.updateVisibility();
             })
         );
+    }
+
+    /** Disables clicking on the status bar item */
+    public disable(): void {
+        // There is an error in the definition of StatusBarItem.command.
+        // The expected type is `string | undefined`, but actual is `string`.
+        // This is workaround.
+        let statusBarItem: any = this.statusBarItem;
+        // Disable clicking.
+        statusBarItem.command = undefined;
+    }
+
+    /** Enables clicking on the status bar item */
+    public enable(): void {
+        this.statusBarItem.command = command;
+    }
+
+    public setOnClicked(onClicked?: () => void): void {
+        this.onClicked = onClicked;
     }
 
     public setText(text: string): void {
